326b1a91df1b3846f68ceaa5a8b2d9b28c0a98f1
Jinze Li <jinze.li@apa.at>
2013-12-10 11:48:48 +0000

    build script for the project
    git-svn-id: svn://svn1.aps.apa.at/CRS/trunk@49486 224852c4-6f55-0410-a66d-b9d8d5c1249d

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+# how $WORKSPACE is set
+# http://hudson.cs.apa.at/env-vars.html/?
+JENKINSPERLINC="$WORKSPACE/crs/src/perllib"
+
+# build the project with libray path
+perl -I "$JENKINSPERLINC" Build.PL
+./Build
+# ./Build install # don't
+prove -I "$JENKINSPERLINC"  -r --timer --formatter=TAP::Formatter::JUnit -l t > test_results.xml
