45791e83e5ec81f3da2804da1612414cb6f5fc1d
Mario Paumann <mario.paumann@apa.at>
2014-09-01 12:42:42 +0000

    CRS-784 : newsml g2 validation works now with newest libxml2@crsbrew
    git-svn-id: svn://svn1.aps.apa.at/CRS/branches/CRS-784@55928 224852c4-6f55-0410-a66d-b9d8d5c1249d

--- t/NewsML_G2_Test_Helpers.pm
+++ t/NewsML_G2_Test_Helpers.pm
@@ -108,14 +108,11 @@ our @keywords = qw(beer vienna prater kolarik schweizerhaus);
 sub validate_g2 {
     my ($dom, $version) = @_;
 
-  SKIP: {
-        skip 'libxml2 before 2.8 reports bogus violation on children of "broader"', 2 if (20800 > XML::LibXML::LIBXML_RUNTIME_VERSION);
-        $version =~ tr/./_/;
-        my $xsd = catfile('t', 'xsds', "NewsML-G2_$version-spec-All-Power.xsd");
-        ok(my $xmlschema = XML::LibXML::Schema->new(location => $xsd), "parsing $version XSD");
+    $version =~ tr/./_/;
+    my $xsd = catfile('t', 'xsds', "NewsML-G2_$version-spec-All-Power.xsd");
+    ok(my $xmlschema = XML::LibXML::Schema->new(location => $xsd), "parsing $version XSD");
 
-        lives_ok(sub {$xmlschema->validate($dom)}, "XML validates against $version XSD");
-    }
+    lives_ok(sub {$xmlschema->validate($dom)}, "XML validates against $version XSD");
 
     return;
 }
