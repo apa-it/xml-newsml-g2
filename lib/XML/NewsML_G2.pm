685704919437d68bc8c13063e5c6764e76d87e25
Philipp Gortan <philipp.gortan@apa.at>
2014-10-22 10:09:41 +0000

    release 0.2.1
    git-svn-id: svn://svn1.aps.apa.at/CRS/branches/CRS-784@57311 224852c4-6f55-0410-a66d-b9d8d5c1249d

--- lib/XML/NewsML_G2.pm
+++ lib/XML/NewsML_G2.pm
@@ -35,7 +35,7 @@ use XML::NewsML_G2::Writer::News_Message;
 use warnings;
 use strict;
 
-use version; our $VERSION = qv('0.2.0');
+use version; our $VERSION = qv('0.2.1');
 
 1;
 
@@ -48,7 +48,7 @@ XML::NewsML_G2 - generate NewsML-G2 news items
 
 =head1 VERSION
 
-0.2.0
+0.2.1
 
 =begin readme
 
