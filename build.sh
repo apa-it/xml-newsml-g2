2d9abd5a366e11ec2acf7520538771934384eea3
Jozef Kutej <jozef.kutej@apa.at>
2019-11-07 16:15:50 +0100

    prevent warnings, skip cover_db

--- build.sh
+++ build.sh
@@ -11,8 +11,8 @@ JENKINSPERLINC="$WORKSPACE/crs/src/perllib"
 # build the project with libray path
 perl -I "$JENKINSPERLINC" Build.PL
 
-./Build
 ./Build distmeta
+./Build
 
 ./Build test merge=1 tap_harness_args=formatter_class=TAP::Formatter::JUnit > test_results.xml
 
